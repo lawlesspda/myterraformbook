7909f68bd4bfc4cc6a2642b4ef4ec540edd3ae57
LAWLESS Paul <paul.lawless@soprasteria.com>
2025-01-06 12:30:15 +0000

    Initial Commit

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+    region = "us-east-1"
+  
+}
+
+resource "aws_instance" "example" {
+ami = "ami-01816d07b1128cd2d"
+instance_type = "t2.micro"
+
+tags = {
+  Name = "terraform-exaple"
+}
+
+}
