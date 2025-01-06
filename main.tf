eace219e5155d4af045c855c65913bacd83c97a2
lawlesspaul <lawlesspaul3737@gmail.com>
2025-01-06 13:12:28 +0000

    Added Web Server

--- main.tf
+++ main.tf
@@ -7,6 +7,16 @@ resource "aws_instance" "example" {
 ami = "ami-01816d07b1128cd2d"
 instance_type = "t2.micro"
 
+user_data = <<-EOF
+    #!/bin/bash
+              yum update -y
+              yum install -y nginx
+              systemctl start nginx
+              systemctl enable nginx
+              echo "<h1>Welcome to My Web Server</h1>" > /usr/share/nginx/html/index.html
+            EOF
+
+
 tags = {
   Name = "terraform-exaple"
 }
