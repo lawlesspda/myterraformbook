7bba253dc6315b2f23479f01a09cbc92c984a7ab
paul lawless <lawlesspaul3737@gmail.com>
2025-01-06 20:49:41 +0000

    newweb

--- main.tf
+++ main.tf
@@ -6,6 +6,8 @@ provider "aws" {
 resource "aws_instance" "example" {
 ami = "ami-01816d07b1128cd2d"
 instance_type = "t2.micro"
+key_name      = "mykey"
+vpc_security_group_ids = [aws_security_group.instance.id]
 
 user_data = <<-EOF
     #!/bin/bash
@@ -21,4 +23,25 @@ tags = {
   Name = "terraform-exaple"
 }
 
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+ingress {
+    description = "Allow 443 from the Internet"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow from all IPs
+  }
+
+  
 }
