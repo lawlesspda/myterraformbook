148d3e10e444d66bd2f9e3bd0895a0743118c97c
lawlesspaul <lawlesspaul3737@gmail.com>
2025-01-07 08:57:48 +0000

    save1

--- main.tf
+++ main.tf
@@ -6,21 +6,47 @@ provider "aws" {
 resource "aws_instance" "example" {
 ami = "ami-01816d07b1128cd2d"
 instance_type = "t2.micro"
+key_name      = "mykey"
+vpc_security_group_ids = [aws_security_group.instance.id]
 
 user_data = <<-EOF
-  user_data = <<-EOF
-              #!/bin/bash
+    #!/bin/bash
               yum update -y
               yum install -y nginx
-              sed -i 's/listen       80;/listen       8080;/g' /etc/nginx/nginx.conf
               systemctl start nginx
               systemctl enable nginx
-              echo "<h1>Welcome to My Web Server on Port 8080</h1>" > /usr/share/nginx/html/index.html
+              echo "<h1>Welcome to My Web Server</h1>" > /usr/share/nginx/html/index.html
             EOF
-user_data_replace_on_change = true
+
 
 tags = {
   Name = "terraform-exaple"
 }
 
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+ingress {
+    description = "Allow 443 from the Internet"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow from all IPs
+  }
+
+   ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow from all IPs
+  }
 }
