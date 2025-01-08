f686b461e5282f2d0f1642bcf65cf58462d7fd2c
paul lawless <lawlesspaul3737@gmail.com>
2025-01-08 21:23:06 +0000

    port 80 recovered

--- main.tf
+++ main.tf
@@ -37,8 +37,8 @@ ingress {
   }
 
   ingress {
-    from_port   = 80802
-    to_port     = 80802
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"] # Allow from all IPs
   }
